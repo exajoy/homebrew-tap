27c798c7f1edc1ad7be87cf80e67452b5da685ba
Thanh Dat Vo <vo9312@gmail.com>
2026-08-19 08:03:55 -0700

    first commit

---
+++ Casks/tad.rb
@@ -0,0 +1,12 @@
+cask 'tad' do
+  version '0.1.0'
+  sha256 'd39ed30ee7590d864fa3e3a126510c717bacc7222397cb2f88246ec2e8de6733'
+
+  url "https://github.com/exajoy/tad/releases/download/v#{version}/TAD_#{version}_aarch64.dmg"
+
+  name 'TAD'
+  desc 'Token Activity Dashboard'
+  homepage 'https://github.com/exajoy/tad'
+
+  app 'TAD.app'
+end
