43f15059e22d8cf01f2d35c7b59542b7811ad993
Thanh Dat Vo <vo9312@gmail.com>
2026-08-19 08:17:49 -0700

    up

--- Casks/tad.rb
+++ Casks/tad.rb
@@ -1,5 +1,5 @@
 cask 'tad' do
-  version '0.1.0'
+  version '0.0.0'
   sha256 'd39ed30ee7590d864fa3e3a126510c717bacc7222397cb2f88246ec2e8de6733'
 
   url "https://github.com/exajoy/tad/releases/download/v#{version}/TAD_#{version}_aarch64.dmg"
